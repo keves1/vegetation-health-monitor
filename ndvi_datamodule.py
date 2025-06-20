a39d338492f7c5d3490d549c00e1e272640291b2
Keenan Eves <keenan573@gmail.com>
2025-06-20 16:07:48 -0600

    modis ndvi pipeline and model training code

---
+++ ndvi_datamodule.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+from torch.utils.data import DataLoader, Subset
+from lightning.pytorch import LightningDataModule
+
+from ndvi_dataset import NDVIDataset
+
+
+class NDVIDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str,
+        batch_size: int = 64,
+        val_split_pct: float = 0.2,
+        test_split_pct: float = 0.2,
+        num_workers: int = 0,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.val_split_pct = val_split_pct
+        self.test_split_pct = test_split_pct
+        self.num_workers = num_workers
+        self.kwargs = kwargs
+
+    def setup(self, stage):
+        dataset = NDVIDataset(**self.kwargs)
+        train_split_pct = 1 - (self.val_split_pct + self.test_split_pct)
+        train_size = int(train_split_pct * len(dataset))
+        val_size = int(self.val_split_pct * len(dataset))
+        train_indices = range(train_size)
+        val_indices = range(train_size, train_size + val_size)
+        test_indices = range(train_size + val_size, len(dataset))
+        self.train_dataset = Subset(dataset, train_indices)
+        self.val_dataset = Subset(dataset, val_indices)
+        self.test_dataset = Subset(dataset, test_indices)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
---
+++ ndvi_dataset.py
@@ -0,0 +1,78 @@
+import os
+from typing import Any
+
+import torch
+from torch.utils.data import Dataset
+import xarray as xr
+
+
+class NDVIDataset(Dataset):
+    data_file_name = "ndvi_processed.zarr"
+
+    def __init__(
+        self,
+        root: str = "data",
+        num_past_steps: int = 3,
+        num_future_steps: int = 1,
+    ) -> None:
+        self.root = root
+        self.num_past_steps = num_past_steps
+        self.num_future_steps = num_future_steps
+
+        data = self._load_data()
+        self.data = data.transpose("time", "y", "x")
+        self.window_size = num_past_steps + num_future_steps
+        self.T, self.H, self.W = self.data.shape
+
+        self.num_time = self.T - self.window_size + 1
+        self.num_spatial = self.H * self.W
+        self.total = self.num_time * self.num_spatial
+
+    def __len__(self) -> int:
+        return self.total
+
+    def __getitem__(self, index: int) -> dict[str, Any]:
+        time_idx = index // self.num_spatial
+        spatial_idx = index % self.num_spatial
+        y = spatial_idx // self.W
+        x = spatial_idx % self.W
+
+        past_steps = self.data.isel(
+            x=x, y=y, time=slice(time_idx, time_idx + self.num_past_steps)
+        )
+        future_steps = self.data.isel(
+            x=x,
+            y=y,
+            time=slice(
+                time_idx + self.num_past_steps,
+                time_idx + self.window_size,
+            ),
+        )
+
+        past_steps_tensor: torch.Tensor = torch.tensor(
+            past_steps.values, dtype=torch.float32
+        ).unsqueeze(-1)
+        future_steps_tensor: torch.Tensor = torch.tensor(
+            future_steps.values, dtype=torch.float32
+        ).unsqueeze(-1)
+
+        mean = past_steps_tensor.mean(dim=0, keepdim=True)
+        std = past_steps_tensor.std(dim=0, keepdim=True)
+        past_steps_normalized = (past_steps_tensor - mean) / (std + 1e-12)
+        future_steps_normalized = (future_steps_tensor - mean) / (std + 1e-12)
+
+        return {
+            "past": past_steps_normalized,
+            "future": future_steps_normalized,
+            "mean": mean,
+            "std": std,
+        }
+
+    def _load_data(self) -> xr.DataArray:
+        pathname = os.path.join(self.root, self.data_file_name)
+        if os.path.exists(pathname):
+            data = xr.open_zarr(pathname)
+            ndvi = data.ndvi
+            return ndvi
+        else:
+            raise FileNotFoundError
