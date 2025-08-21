5ec72ed9a4d7fc85b2155adea16f6750062a98bd
Keenan Eves <keenan573@gmail.com>
2025-08-20 18:58:53 -0600

    using landsat and storing raw and processed.

--- pipelines/ndvi_pipeline.py
+++ pipelines/ndvi_pipeline.py
@@ -2,8 +2,10 @@ import json
 import os
 from datetime import date, datetime, time
 
+import numpy as np
 import pystac_client
 import s3fs
+import xarray as xr
 from dotenv import load_dotenv
 from odc.stac import load
 from planetary_computer import sign_url
@@ -33,10 +35,16 @@ if fs.exists(END_DATE_PATH):
 else:
     raise FileNotFoundError(f"{END_DATE_PATH} was not found.")
 
+if fs.exists(f"{ZARR_PATH}/zarr.json"):
+    store = s3fs.S3Map(root=ZARR_PATH, s3=fs, check=False)
+    ds = xr.open_zarr(store)
+else:
+    raise FileNotFoundError(f"{ZARR_PATH} was not found.")
+
 catalog = pystac_client.Client.open(
     "https://planetarycomputer.microsoft.com/api/stac/v1/"
 )
-collection = "modis-09Q1-061"
+collection = "landsat-c2-l2"
 bbox = bbox
 end_date = datetime.combine(date.today(), time()).strftime("%Y-%m-%dT%H:%M:%SZ")
 
@@ -53,7 +61,7 @@ if len(items) == 0:
 else:
     unique_dates = sorted(
         set(
-            datetime.fromisoformat(item.properties["end_datetime"])
+            datetime.fromisoformat(item.properties["datetime"])
             .date()
             .strftime("%Y-%m-%d")
             for item in items
@@ -63,27 +71,38 @@ else:
     with fs.open(END_DATE_PATH, "w") as file:
         file.write(end_date)
 
-data = load(
+new_data = load(
     items,
-    bands=["red", "nir08", "sur_refl_qc_250m"],
+    bands=["red", "nir08", "qa_pixel"],
     bbox=bbox,
     chunks={"x": 2048, "y": 2048},
+    resolution=300,
     groupby="solar_day",
     patch_url=sign_url,
 )
 
-# Mask out low-quality pixels
-mask = data.sur_refl_qc_250m.where((data.sur_refl_qc_250m & 0b11110000) == 0)
-data = data.where(mask)
+# Mask out nodata and cloud pixels
+# Bit 3 is cloud shadow, bit 4 is cloud, and bit 0 is nodata
+mask_bits = 0b00011001
 
-ndvi = (data.nir08 - data.red) / (data.nir08 + data.red)
-data["ndvi"] = ndvi.clip(-1, 1)
-data = data.drop_vars(["red", "nir08", "sur_refl_qc_250m"])
-data = data.compute()
+mask = (new_data.qa_pixel & mask_bits) != 0
 
-if fs.exists(f"{ZARR_PATH}/zarr.json"):
-    store = s3fs.S3Map(root=ZARR_PATH, s3=fs, check=False)
-    data.to_zarr(store, mode="a", append_dim="time")
-    print("New data added to zarr store.")
-else:
-    raise FileNotFoundError(f"{ZARR_PATH} was not found.")
+new_data = new_data.where(~mask, other=np.nan).drop_vars("qa_pixel")
+
+ndvi = (new_data.nir08 - new_data.red) / (new_data.nir08 + new_data.red)
+new_data["ndvi"] = ndvi.clip(-1, 1)
+new_data = new_data.drop_vars(["red", "nir08"])
+new_data = new_data.compute()
+
+eight_day = new_data.ndvi.resample(time="8D").max()
+filled = eight_day.bfill("time").ffill("time")
+
+new_data = xr.Dataset(
+    {
+        "ndvi_8d_raw": eight_day,
+        "ndvi_8d_processed": filled,
+    }
+)
+
+new_data.to_zarr(store, mode="a", append_dim="time")
+print("New data added to zarr store.")
