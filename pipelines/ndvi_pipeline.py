f847d366e64801b8b799978a377ed513ad77d3de
Keenan Eves <keenan573@gmail.com>
2025-08-23 13:41:47 -0600

    added improved updating and gap filling for new data.

--- pipelines/ndvi_pipeline.py
+++ pipelines/ndvi_pipeline.py
@@ -14,10 +14,7 @@ from shapely.geometry import shape
 load_dotenv()
 BUCKET_NAME = os.environ["S3_BUCKET"]
 AOI_PATH = f"{BUCKET_NAME}/aoi.geojson"
-END_DATE_PATH = f"{BUCKET_NAME}/last_end_date.txt"
 ZARR_PATH = f"{BUCKET_NAME}/ndvi_processed.zarr"
-
-# dask_client = DaskClient()
 
 fs = s3fs.S3FileSystem()
 
@@ -29,15 +26,11 @@ if fs.exists(AOI_PATH):
 else:
     raise FileNotFoundError(f"{AOI_PATH} was not found.")
 
-if fs.exists(END_DATE_PATH):
-    with fs.open(END_DATE_PATH, "r") as file:
-        start_date = file.readline().strip()
-else:
-    raise FileNotFoundError(f"{END_DATE_PATH} was not found.")
-
 if fs.exists(f"{ZARR_PATH}/zarr.json"):
     store = s3fs.S3Map(root=ZARR_PATH, s3=fs, check=False)
     ds = xr.open_zarr(store)
+    start_date = ds.time[-1].values
+    print(f"Last 5 dates in {ZARR_PATH} before update: {ds.time.values[-5:]}")
 else:
     raise FileNotFoundError(f"{ZARR_PATH} was not found.")
 
@@ -68,8 +61,6 @@ else:
         )
     )
     print(f"STAC search found {len(items)} items for dates {unique_dates}.")
-    with fs.open(END_DATE_PATH, "w") as file:
-        file.write(end_date)
 
 new_data = load(
     items,
@@ -94,15 +85,38 @@ new_data["ndvi"] = ndvi.clip(-1, 1)
 new_data = new_data.drop_vars(["red", "nir08"])
 new_data = new_data.compute()
 
-eight_day = new_data.ndvi.resample(time="8D").max()
-filled = eight_day.bfill("time").ffill("time")
+# origin=start_date so that the previous last time window is recomputed with any new data
+eight_day = new_data.ndvi.resample(time="8D", origin=start_date).max()
 
 new_data = xr.Dataset(
     {
         "ndvi_8d_raw": eight_day,
-        "ndvi_8d_processed": filled,
+        "ndvi_8d_processed": eight_day,
     }
 )
 
-new_data.to_zarr(store, mode="a", append_dim="time")
-print("New data added to zarr store.")
+overlap = new_data.sel(time=slice(start_date, start_date))
+
+overlap.drop_vars(["y", "x", "spatial_ref"]).to_zarr(
+    store, mode="a", region={"time": slice(-1, None)}
+)
+print(f"Dates updated: {overlap.time.values}")
+new = new_data.isel(time=slice(1, None))
+new.to_zarr(store, mode="a", append_dim="time")
+print(f"Dates added: {new.time.values}")
+
+# Fill the newly added data using the last filled date
+ds = xr.open_zarr(store)
+last_filled_date = start_date - np.timedelta64(8, "D")
+filled = (
+    ds["ndvi_8d_processed"]
+    .sel(time=slice(last_filled_date, None))
+    .bfill("time")
+    .ffill("time")
+)
+filled.drop_vars(["y", "x", "spatial_ref"]).to_zarr(
+    store, mode="a", region={"time": slice(len(ds.time) - len(filled.time), None)}
+)
+print(f"Dates filled: {filled.time.values}")
+print("Finished adding data to zarr store.")
+print(f"Last 5 dates in {ZARR_PATH} after update: {ds.time.values[-5:]}")
