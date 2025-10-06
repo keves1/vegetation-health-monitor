904f9da2b77a168b2c90eb01465dde36d638dcab
Keenan Eves <keenan573@gmail.com>
2025-10-06 16:35:55 -0600

    add features to app

--- ui/app.py
+++ ui/app.py
@@ -2,6 +2,7 @@ import os
 
 import boto3
 import leafmap.foliumap as leafmap
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,27 +10,36 @@ BUCKET_NAME = os.environ["S3_BUCKET"]
 
 
 class VegetationHealthMonitor:
-    def display(self):
+    def __init__(self):
+        st.set_page_config(layout="wide", page_title="Vegetation Health Monitor")
+
+    def generate_presigned_url(self, key):
         s3_client = boto3.client("s3")
-        recent_trend_url = s3_client.generate_presigned_url(
+        return s3_client.generate_presigned_url(
             "get_object",
             Params={
                 "Bucket": BUCKET_NAME,
-                "Key": "COG/ndvi_recent_trend.tif",
+                "Key": key,
             },
             ExpiresIn=3600,
         )
-        forecast_trend_url = s3_client.generate_presigned_url(
-            "get_object",
-            Params={
-                "Bucket": BUCKET_NAME,
-                "Key": "COG/ndvi_forecast_trend.tif",
-            },
-            ExpiresIn=3600,
+
+    def display(self):
+        st.title("Vegetation Health Monitor")
+        st.write(
+            "The Vegetation Health Monitor forecasts NDVI using Landsat imagery to show predicted trends in vegetation health. "
+            "NDVI is widely used for monitoring the health and growth stages of ecosystems and crops, which is important for "
+            "understanding the effects of drought, desertification, and other natural or human-caused events. Forecasting NDVI allows for advanced operational planning and intervention."
         )
+
+        recent_trend_url = self.generate_presigned_url("COG/ndvi_recent_trend.tif")
+        forecast_trend_url = self.generate_presigned_url("COG/ndvi_forecast_trend.tif")
+
         m = leafmap.Map(
             center=[13.541243890565807, -2.430867732749294],
             zoom=10,
+            draw_control=False,
+            search_control=False,
         )
         m.add_basemap("HYBRID")
         custom_cmap = {
@@ -50,6 +60,31 @@ class VegetationHealthMonitor:
             colormap=custom_cmap,
             name="Forecast Trend",
             fit_bounds=True,
+        )
+        m.add_vector(
+            filename="Burkina_Faso_ADM2_simplified.simplified.geojson",
+            zoom_to_layer=False,
+        )
+        m.add_vector(
+            filename="Mali_ADM2_simplified.simplified.geojson",
+            zoom_to_layer=False,
+        )
+        m.add_legend(
+            title="Vegetation Growth Trend",
+            labels=[
+                "Bare/Sparse Vegetation",
+                "No Change",
+                "Green-up",
+                "Senescence/Browning",
+                "Peak Growth",
+            ],
+            colors=[
+                "#8b6f47",
+                "#a9a9a9",
+                "#45e640",
+                "#dc4133",
+                "#ffd700",
+            ],
         )
         m.to_streamlit(height=600)
 
