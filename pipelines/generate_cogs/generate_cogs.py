b4cd32b79bd1db22265c512ab1fdadb52b51df52
Keenan Eves <keenan573@gmail.com>
2025-10-31 11:22:48 -0600

    improved thresholds and include class for insufficient data.

--- pipelines/generate_cogs/generate_cogs.py
+++ pipelines/generate_cogs/generate_cogs.py
@@ -16,11 +16,60 @@ NUM_PAST_STEPS = 3
 RASTER_LOCAL_DIR = "/tmp"
 RASTER_PREFIX = "COG"
 
+
+def calculate_slope(da):
+    fit = da.polyfit(dim="time", deg=1)
+    slope = fit.polyfit_coefficients.sel(degree=1)
+    slope = (
+        slope * 1e9 * 60 * 60 * 24 * 8
+    )  # convert from slope based on nanoseconds to 8 day intervals
+    return slope
+
+
+def percent_missing(da):
+    return da.isnull().sum(dim="time") / da.sizes["time"] * 100
+
+
+def classify_trend(last_timestep, slope, percent_missing):
+    # Thresholds
+    ndvi_thresh_bare = 0.08  # based on typical NDVI for barren in this region
+    ndvi_thresh_peak = 0.26  # 95th percentile NDVI cropland/grassland in this region
+    slope_thresh = 0.001  # tolerance for "no change"
+    percent_missing_thresh = 0.5
+
+    # Start with all zeros. Represents bare/sparse vegetation
+    classes = xr.full_like(last_timestep, fill_value=0, dtype=np.int8)
+
+    # No change (NDVI > bare threshold slope near zero)
+    no_change_mask = (last_timestep > ndvi_thresh_bare) & (np.abs(slope) < slope_thresh)
+    classes = classes.where(~no_change_mask, 1)
+
+    # Greening (NDVI > bare threshold and slope > 0)
+    greening_mask = (last_timestep > ndvi_thresh_bare) & (slope > slope_thresh)
+    classes = classes.where(~greening_mask, 2)
+
+    # Browning/senescence (NDVI > bare threshold and slope < 0)
+    browning_mask = (last_timestep > ndvi_thresh_bare) & (slope < -slope_thresh)
+    classes = classes.where(~browning_mask, 3)
+
+    # Peak growth (NDVI > peak growth threshold)
+    peak_mask = last_timestep > ndvi_thresh_peak
+    classes = classes.where(~peak_mask, 4)
+
+    # Don't classify trend if too much missing data
+    missing_mask = percent_missing > percent_missing_thresh
+    classes = classes.where(~missing_mask, 5)
+
+    return classes
+
+
 fs = s3fs.S3FileSystem()
 
 if fs.exists(f"{NDVI_ZARR_PATH}/zarr.json"):
     ndvi_store = s3fs.S3Map(root=NDVI_ZARR_PATH, s3=fs, check=False)
-    ndvi = xr.open_zarr(ndvi_store).ndvi_8d_processed
+    ndvi = xr.open_zarr(ndvi_store)
+    ndvi_processed = ndvi.ndvi_8d_processed
+    ndvi_raw = ndvi.ndvi_8d_raw
 else:
     raise FileNotFoundError(f"{NDVI_ZARR_PATH} was not found.")
 
@@ -32,82 +81,30 @@ else:
 
 s3 = boto3.client("s3")
 
-### Trend plot
-ndvi_recent = ndvi.isel(time=slice(-4, -1))
+# Recent Trend
+ndvi_recent = ndvi_processed.isel(time=slice(-4, -1))
 ndvi_last = ndvi_recent.isel(time=-1)
+ndvi_recent_raw = ndvi_raw.isel(time=slice(-4, -1))
+recent_percent_missing = percent_missing(ndvi_recent_raw)
 
-fit = ndvi_recent.polyfit(dim="time", deg=1)
-slope = fit.polyfit_coefficients.sel(degree=1)
-slope = (
-    slope * 1e9 * 60 * 60 * 24 * 8
-)  # convert from slope based on nanoseconds to 8 day intervals
-
-# Thresholds
-ndvi_thresh_sparse = 0.1
-ndvi_thresh_peak = 0.25
-slope_thresh = 0.01  # tolerance for "stable"
-
-# Start with all zeros
-classes = xr.full_like(ndvi_last, fill_value=0, dtype=np.int8)
-
-# Bare/sparse vegetation
-classes = classes.where(~(ndvi_last <= ndvi_thresh_sparse), 0)
-
-# Stable (slope near zero, regardless of NDVI)
-stable_mask = np.abs(slope) < slope_thresh
-classes = classes.where(~stable_mask, 1)
-
-# Greening (NDVI > 0.1 and slope > 0)
-greening_mask = (ndvi_last > ndvi_thresh_sparse) & (slope > slope_thresh)
-classes = classes.where(~greening_mask, 2)
-
-# Browning/senescence (NDVI > 0.1 and slope < 0)
-browning_mask = (ndvi_last > ndvi_thresh_sparse) & (slope < -slope_thresh)
-classes = classes.where(~browning_mask, 3)
-
-# Peak growth (NDVI > preak growth threshold)
-ndvi_prev = ndvi.isel(time=slice(-6, -3))
-fit_prev = ndvi_prev.polyfit(dim="time", deg=1)
-slope_prev = fit_prev.polyfit_coefficients.sel(degree=1)
-slope_prev = slope_prev * 1e9 * 60 * 60 * 24 * 8
-peak_mask = ndvi_last > ndvi_thresh_peak
-classes = classes.where(~peak_mask, 4)
+slope = calculate_slope(ndvi_recent)
+recent_trend = classify_trend(ndvi_last, slope, recent_percent_missing)
 
 raster_name = "ndvi_recent_trend.tif"
 raster_path = Path(RASTER_LOCAL_DIR) / raster_name
-classes.rio.to_raster(raster_path=raster_path, driver="COG")
+recent_trend.rio.to_raster(raster_path=raster_path, driver="COG")
 s3.upload_file(raster_path, BUCKET_NAME, f"{RASTER_PREFIX}/{raster_name}")
+print("Created recent trend raster.")
 
+# Forecasted Trend
 pred_recent = preds.isel(time=slice(-3, None))
 pred_last = pred_recent.isel(time=-1)
 
-fit_pred = pred_recent.polyfit(dim="time", deg=1)
-slope_pred = fit_pred.polyfit_coefficients.sel(degree=1)
-slope_pred = slope_pred * 1e9 * 60 * 60 * 24 * 8
-
-# Start with all zeros
-classes = xr.full_like(pred_last, fill_value=0, dtype=np.int8)
-
-# Bare/sparse vegetation
-classes = classes.where(~(pred_last <= ndvi_thresh_sparse), 0)
-
-# Stable (slope near zero, regardless of NDVI)
-stable_mask = np.abs(slope_pred) < slope_thresh
-classes = classes.where(~stable_mask, 1)
-
-# Greening (NDVI > 0.1 and slope > 0)
-greening_mask = (pred_last > ndvi_thresh_sparse) & (slope_pred > slope_thresh)
-classes = classes.where(~greening_mask, 2)
-
-# Browning/senescence (NDVI > 0.1 and slope < 0)
-browning_mask = (pred_last > ndvi_thresh_sparse) & (slope_pred < -slope_thresh)
-classes = classes.where(~browning_mask, 3)
-
-# Peak growth (NDVI > peak growth threshold)
-peak_mask = pred_last > ndvi_thresh_peak
-classes = classes.where(~peak_mask, 4)
+slope = calculate_slope(pred_recent)
+forecast_trend = classify_trend(pred_last, slope, recent_percent_missing)
 
 raster_name = "ndvi_forecast_trend.tif"
 raster_path = Path(RASTER_LOCAL_DIR) / raster_name
-classes.rio.to_raster(raster_path=raster_path, driver="COG")
+forecast_trend.rio.to_raster(raster_path=raster_path, driver="COG")
 s3.upload_file(raster_path, BUCKET_NAME, f"{RASTER_PREFIX}/{raster_name}")
+print("Created forecasted trend raster.")
